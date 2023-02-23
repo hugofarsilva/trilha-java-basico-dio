3ef346f635309d02220c0408905e718fcb011c14
hugofarsilva <sucao9@gmail.com>
2023-02-23 15:38:50 -0300

    alterações arquivo SobreMim

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,8 +1,8 @@
 public class AboutMe {
     public static void main(String[] args) {
         //os argumentos começam com indice 0
-        String nome = args [0];
-        String sobrenome = args [1];
+        String nome = args[0];
+        String sobrenome = args[1];
         int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
         double altura = Double.valueOf(args[3]);
 
